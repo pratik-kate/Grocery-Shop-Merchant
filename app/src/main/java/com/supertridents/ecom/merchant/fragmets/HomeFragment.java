5b565b3814ef4e7a864eef9b16528f78d66693df
pratikkate3 <pratikrkate@gmail.com>
2021-02-20 14:19:48 +0530

    Adding Categories via Merchant App

--- app/src/main/java/com/supertridents/ecom/merchant/fragmets/HomeFragment.java
+++ app/src/main/java/com/supertridents/ecom/merchant/fragmets/HomeFragment.java
@@ -1,14 +1,22 @@
 package com.supertridents.ecom.merchant.fragmets;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,18 +27,27 @@ import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.supertridents.ecom.merchant.R;
 import com.supertridents.ecom.merchant.adapter.MenuViewHolder;
 import com.supertridents.ecom.merchant.model.HomeModel;
-
 import java.util.ArrayList;
 import java.util.HashMap;
+import static android.app.Activity.RESULT_OK;
 
 public class HomeFragment extends Fragment implements BaseSliderView.OnSliderClickListener,
         ViewPagerEx.OnPageChangeListener {
@@ -44,6 +61,15 @@ public class HomeFragment extends Fragment implements BaseSliderView.OnSliderCli
     private MenuViewHolder menuViewHolder;
     private Context mcontext;
     private ShimmerFrameLayout shimmerFrameLayout;
+
+    FloatingActionButton addCat;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    TextInputLayout catName;
+    TextView chooseImage,addcat;
+    Uri saveUri;
+    private final int PICK_IMAGE_REQUEST = 1;
+    HomeModel category;
 
 
     public HomeFragment() {
@@ -102,8 +128,97 @@ public class HomeFragment extends Fragment implements BaseSliderView.OnSliderCli
         getDataFromFirebase();
 
 
+        //Adding Categories
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        addCat = view.findViewById(R.id.addCat);
+        addCat.setOnClickListener(v -> {
+
+            final Dialog dialog2 = new Dialog(getContext());
+            dialog2.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+            dialog2.setContentView(R.layout.category);
+            dialog2.setCancelable(true);
+            WindowManager.LayoutParams lp2 = new WindowManager.LayoutParams();
+            lp2.copyFrom(dialog2.getWindow().getAttributes());
+            lp2.width = WindowManager.LayoutParams.MATCH_PARENT;
+            lp2.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+            catName = (dialog2.findViewById(R.id.cat_name));
+
+
+            chooseImage = dialog2.findViewById(R.id.chooseImage);
+            addcat = dialog2.findViewById(R.id.add);
+
+            chooseImage.setOnClickListener(v1 -> {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"select Picture"),PICK_IMAGE_REQUEST);
+            });
+            addcat.setOnClickListener(v1 -> {
+                if(saveUri != null){
+                    String name = catName.getEditText().getText().toString().trim();
+                    ProgressDialog progressDialog = new ProgressDialog(getContext());
+                    progressDialog.setMessage("Uploading Image....");
+                    progressDialog.show();
+                    StorageReference filepath = storageReference.child("category").child(saveUri.getLastPathSegment());
+                    filepath.putFile(saveUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    Uri download=uri;
+
+                                    HashMap<String ,Object> map = new HashMap<>();
+                                    map.put("image",download.toString());
+                                    map.put("name",name);
+
+                                    FirebaseDatabase.getInstance().getReference().child("category").push().setValue(map)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    //Toast.makeText(getContext(), "Uploaded", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            progressDialog.dismiss();
+                                        }
+                                    }).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toast.makeText(getContext(), "Uploaded", Toast.LENGTH_SHORT).show();
+                                            progressDialog.dismiss();
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+
+
+                }
+
+            });
+
+
+            dialog2.show();
+            dialog2.getWindow().setAttributes(lp2);
+        });
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data!=null && data.getData() != null){
+            saveUri = data.getData();
+            chooseImage.setText("Image Selected");
+            chooseImage.setClickable(false);
+        }
     }
 
     private void getDataFromFirebase() {
